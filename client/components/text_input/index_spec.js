d31d23c74d82ee5ac7174fcbd3e0df96a700fe7d
Todd Morningstar <techarchist@gmail.com>
2017-11-11 11:06:50 -0500

    Add new _spec file, as per test suite config

---
+++ client/components/text_input/index_spec.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import TextInput from './index';
+
+describe('<TextInput />', () => {
+  it('produces a div container with a single input', () => {
+    const wrapper = shallow(<TextInput />);
+
+    expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  it('produces an input of type text', () => {
+    const wrapper = shallow(<TextInput />);
+
+    expect(wrapper.find('input[type="text"]').length).toEqual(1);
+  });
+});
