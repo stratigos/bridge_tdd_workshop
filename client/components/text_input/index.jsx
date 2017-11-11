3afaba2e2b348c58360de85c3fe1ba501c495774
Todd Morningstar <techarchist@gmail.com>
2017-11-11 11:12:48 -0500

    Make first tests pass
     * Implement the TextInput component, and add the needed input element

--- client/components/text_input/index.jsx
+++ client/components/text_input/index.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 
 export default class TextInput extends Component {
-  render() {}
+  render() {
+    return(
+      <div className='todds-test-input-container'>
+        <input type='text' name='todds-test-input' placeholder='type stuff here!' />
+      </div>
+    );
+  }
 }
